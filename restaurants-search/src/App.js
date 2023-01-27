86a4bcee0d8ba4ca9eea255b6df855cf9923ad21
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2023-01-26 22:17:33 -0300

    .

--- restaurants-search/src/App.js
+++ restaurants-search/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
 import Home from './pages/Home';
 import theme from './theme';
-import { ThemeProvider } from 'styled-components';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home/>
+      <Reset />
+      <Home />
     </ThemeProvider>
   );
 }
